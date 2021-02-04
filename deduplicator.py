6a7f302a0d91e34b8aaee3a6e75ff32508451c49
bddin <61153230+bddin@users.noreply.github.com>
2021-02-04 20:56:29 +0600

    Added redis (#17)
    * Added redis
    
    * Added redis
    
    * Update deduplicator.py

--- deduplicator.py
+++ deduplicator.py
@@ -1,12 +1,13 @@
 from string import punctuation
 
+import redis
 from simhash import Simhash
 
 
-class Deduplicator():
-    def __init__(self):
-        self.hashs = set()
+rds = redis.Redis(host='localhost', port=6379, db=0)  # noqa: WPS432
 
+
+class Deduplicator():
     def preprocess(self, string):
         table = str.maketrans('', '', punctuation)
         list_string = string.translate(table).lower().split()
@@ -16,7 +17,7 @@ class Deduplicator():
     def isduplicate(self, string):
         string = self.preprocess(string)
         hsh = Simhash(string).value
-        if hsh not in self.hashs:
-            self.hashs.add(hsh)
+        if not rds.exists(hsh):
+            rds.set(hsh, 1)
             return False
         return True
